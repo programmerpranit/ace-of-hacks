d4620cb5e690ff7f83b8256d08821874949766e1
Pranit Patil <dev.pranitpatil@gmail.com>
2024-02-21 14:35:52 +0530

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,34 +17,34 @@ export default function Home(): JSX.Element {
 
         <Image
           src={"/images/acpce-dark-logo.png"}
-          width={250}
-          height={250}
+          width={200}
+          height={200}
           alt=""
-          className="absolute left-5 top-5"
+          className="absolute left-5 top-5 max-md:w-40"
         />
         <Image
           src={"/images/blu-circles.png"}
           width={150}
           height={150}
           alt=""
-          className="animate-cross absolute right-0 top-0"
+          className="animate-cross absolute right-0 top-0 max-md:w-24"
         />
         <Image
           src={"/images/blu-decor.png"}
-          className="transition-duration-5 absolute -left-20 animate-spin"
+          className="transition-duration-5 absolute -left-20 animate-spin max-md:bottom-10 max-md:w-36"
           width={180}
           height={180}
           alt=""
         />
         <Image
-          className="absolute left-[40vw] top-10 animate-bounce"
+          className="absolute bottom-28 right-5 animate-bounce md:left-[40vw] md:top-10"
           src={"/images/yellow-deco.webp"}
           width={100}
           height={100}
           alt=""
         />
 
-        <h1 className="text-9xl font-extrabold">
+        <h1 className="text-5xl font-extrabold md:text-9xl">
           {" "}
           <span className="text-primary"> Ace </span>of{" "}
           <span className="text-secondary"> Hacks</span>
@@ -52,7 +52,9 @@ export default function Home(): JSX.Element {
         </h1>
 
         <div className="mt-10 animate-pulse rounded-full border border-secondary px-20 py-2 ">
-          <p className="spacing text-4xl font-semibold">COMING SOON</p>
+          <p className="spacing text-xl font-semibold md:text-4xl">
+            COMING SOON
+          </p>
         </div>
 
         <div className="absolute bottom-10  mt-10 rounded-full border border-secondary px-10 py-1 ">
