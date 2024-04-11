0272d2fd8e63a02dc0cdab1f02ca58506150c0bb
Pranit Patil <dev.pranitpatil@gmail.com>
2024-04-11 15:38:39 +0530

    cert main

--- src/app/admin/actions.ts
+++ src/app/admin/actions.ts
@@ -23,10 +23,19 @@ export const sendMail = async (cid: string): Promise<boolean> => {
   const mailOptions = {
     from: "Pranit Patil <dev@pranitpatil.com>", // sender address
     to: participant.email,
-    subject: ` Download your certificate`, // Subject line
-    html: `<p> Hello Hacker,</p>
-<p>Your certificate is ready get it form this link https://aceofhacks.tech/cert/${participant._id}</p> 
-`,
+    subject: `Your Hackathon Participation Certificate Is Here! `, // Subject line
+
+    html: `
+    <p>Hey ${participant.fname}</p>
+
+<p>We hope you're still riding high on the hackathon vibes! As promised, your participation certificate for Ace of Hacks is ready for download. Click here to grab it: https://aceofhacks.tech/cert/${participant._id}</p>
+
+<p>But wait, there's more! We've also compiled an awesome album of photos capturing the best moments from the hackathon. Relive the excitement, the teamwork, and the unforgettable memories! Check out the photo album here: https://photos.app.goo.gl/uumpmfK6tky6KeRm8</p>
+
+<p>Now, it's your time to shine! Share your hackathon experience, along with your participation certificate, on LinkedIn or Twitter. Let the world know about your incredible journey and the amazing project you worked on!</p>
+
+<p>Keep inspiring, keep innovating, and keep shining bright! &#55357;&#56491;</p>
+    `,
   };
 
   const info = await transporter.sendMail(mailOptions);
